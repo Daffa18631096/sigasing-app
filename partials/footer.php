ada021d276beec31d0196028c668225259a0fc8b
Daffa18631096 <muhammaddaffa0007@gmail.com>
2022-02-07 11:29:16 +0800

    modifikasi koreksi koding semua file

--- partials/footer.php
+++ partials/footer.php
@@ -1,9 +1,7 @@
-<!-- Main Footer -->
 <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-block">
-        <b>Version</b>0.1.0
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2022 <a href="#">Praktikum Web</a>.</strong> All rights reserved.
-  </footer>
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> 0.1.0
+  </div>
+  <strong>Copyright &copy; 2022 <a href="https://adminlte.io">Praktikum Web</a>.</strong> All rights reserved.
+</footer>
+</div>
--- partials/routes.php
+++ partials/routes.php
@@ -1,19 +1,20 @@
 <?php
 if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-    switch ($page) {
-        case '':
-        case 'home':
-            file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
-            break;
+  $page = $_GET['page'];
 
-        case 'lokasiread':
-            file_exists('pages/admin/lokasiread.php') ? include 'pages/admin/lokasiread.php' : include "page/404.php";
-            break;
-            
-        default:
-            include "pages/404.php";
-    }
+  switch ($page) {
+    case '';
+    case 'home':
+      file_exists('pages/home.php') ? include 'page/home/php' : include "pages/404.php";
+      break;
+    case 'lokasiread':
+      file_exists('pages/admin/lokasiread.php') ? include
+        'pages/admin/lokasiread.php' : include "pages/404.php";
+      break;
+
+    default:
+      include "pages/404.php";
+  }
 } else {
-    include "pages/home.php";
+  include "pages/home.php";
 }
