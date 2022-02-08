af4b26145073ef32c45cebfde9aed882556523de
Daffa18631096 <muhammaddaffa0007@gmail.com>
2022-02-09 06:29:58 +0800

    membuat perintah create data

--- pages/admin/lokasicreate.php
+++ pages/admin/lokasicreate.php
@@ -1,5 +1,40 @@
+<?php
+if (isset($_POST['button_create'])) {
+
+    $database = new Database();
+    $db = $database->getConnection();
+    $validateSql = "INSERT INTO lokasi SET nama_lokasi = '" . $_POST['nama_lokasi'] . "'";
+    $stmt = $db->prepare($validateSql);
+    $stmt->bindParam(1, $_POST['nama_lokasi']);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+?>
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
+            <h5><i class="icon fas fa-ban"></i>gagal</h5>
+            Nama lokasi sama sudah ada
+        </div>
+<?php
+    } else {
+        $insertSQL = "INSERT INTO lokasi SET nama_lokasi = ?";
+        $stmt = $db->prepare($insertSQL);
+        $stmt->bindParam(1, $_POST['nama_lokasi']);
+        if ($stmt->execute()) {
+            $_SESSION['hasil'] = true;
+            $_SESSION['pesan'] = "berhasil simpan data";
+        } else {
+            $_SESSION['hasil'] = false;
+            $_SESSION['pesan'] = "gagal simpan data";
+        }
+        echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";
+    }
+}
+
+
+?>
+
 <section class="content-header">
-    <div class="containerfluid">
+    <div class="container-fluid">
         <div class="row mb2">
             <div class="col-sm-6">
                 <h1>Tambah Data Lokasi</h1>
